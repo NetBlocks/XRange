1b908a339d83706c970162df5e05267f56325f2b
NetBlocks <admin@netblocks.eu>
2015-11-22 10:59:20 +0200

    HSE_VALUE
    HSE_VALUE set to 16000000

--- Examples/inc/stm32l1xx.h
+++ Examples/inc/stm32l1xx.h
@@ -103,7 +103,7 @@
         can define the HSE value in your toolchain compiler preprocessor.
   */
 #if !defined  (HSE_VALUE)
-#define HSE_VALUE    ((uint32_t)8000000) /*!< Value of the External oscillator in Hz */
+#define HSE_VALUE    ((uint32_t)16000000) /*!< Value of the External oscillator in Hz */
 #endif
 
 /**
